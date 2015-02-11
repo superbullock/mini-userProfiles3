37aae168494d65322d964023b8fa1cdf9c690782
DANIEL BULLOCK <DANIEL.I.BULLOCK@gmail.com>
2015-02-10 19:24:18 -0700

    "finished mini project"

--- service.js
+++ service.js
@@ -1,12 +1,23 @@
 var app = angular.module('userProfiles');
 
-app.service('mainService', function($http, $q) {
+app.service('mainService', function ($http, $q) {
 
+  var deffered = $q.defer();
 
-  this.getUsers = function() {
-    return $http({
-        method: 'GET',
-        url: 'http://reqr.es/api/users?page=1'
-    })
+  this.getUsers = function () {
+    $http({
+      method: 'GET',
+      url: 'http://reqr.es/api/users?page=1'
+    }).then(function (response) {
+      response = response.data.data;
+      //      for (var i = 0; i < response.length; i++) {
+      //        response[i].first_name = 'Ralf';
+      //      };
+      response.forEach(function (user, index) {
+        user.first_name = 'Ralf';
+      });
+      deffered.resolve(response);
+    });
+    return deffered.promise;
   }
 });
